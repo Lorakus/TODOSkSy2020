4e603945810de6b6e2c8646b79301499aada0759
Karol <rogoza.karol98@gmail.com>
2020-05-02 14:02:54 +0200

    pages plus navbar (created)

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/add">Add</nuxt-link>
+        <nuxt-link to="/edit">Edit</nuxt-link>
+        <nuxt-link to="/impressum">About</nuxt-link>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
---
+++ pages/impressum.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        impressum
+ 
+    </div>
+</template>
