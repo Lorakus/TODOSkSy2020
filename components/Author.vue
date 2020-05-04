48e940ad26ddef5de9abd9bd6c3805603e88e806
Marii19 <m.trzeciakiewicz14@gmail.com>
2020-05-04 13:56:18 +0200

    troche wiecej css

--- components/Author.vue
+++ components/Author.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class=" border-solid border-gray-600 bg-red-100 border-4 w-1/3 flex">
-        <div class = "w-24 h-32 " >
-            <slot  name="photo"></slot>
-        </div>
-        <div class="">
-            <slot name="name"></slot>
-        </div>
-        <div>
-            <slot name="description"></slot>
+    <div class=" border-solid border-gray-600  border-2 w-1/3 flex p-4 m-2 rounded-full">
+        <div class="flex flex-col content-center p-4">
+            <div class="">
+                <slot name="name"></slot>
+            </div>
+            <div>
+                <slot name="description"></slot>
+            </div>
         </div>
     </div>
 </template>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -4,17 +4,19 @@
             <h1>Welcome</h1>
             <h2>This page was created by: </h2>
         </div>
-        <Author >
-            <img src="@/assets/img/ziemniak.jpg" slot="photo"> 
-            <p slot="name"> Mariusz Trzeciakiewicz </p>
-            <p slot="description"> Computer science student at TUB </p>
-        </Author>
+        <div class="flex flex-col content-center content-around">
+            <Author >
+                <img src="@/assets/img/ziemniak.jpg" slot="photo"> 
+                <p slot="name"> Mariusz Trzeciakiewicz </p>
+                <p slot="description"> Computer science student at TUB </p>
+            </Author>
 
-        <Author>
-            <img src="@/assets/img/ziemniak.jpg" slot="photo">
-            <p slot="name"> Karol Rogoza </p>
-            <p slot="description"> Computer science student at TUB </p>
-        </Author>
+            <Author>
+                <img src="@/assets/img/ziemniak.jpg" slot="photo">
+                <p slot="name"> Karol Rogoza </p>
+                <p slot="description"> Computer science student at TUB </p>
+            </Author>
+        </div>
     </div>
 </template>
 
