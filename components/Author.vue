b15a76944f57c7c692ab88b8608c98f31fccfea9
Marii19 <m.trzeciakiewicz14@gmail.com>
2020-05-04 09:49:37 +0200

    impressum/author component

---
+++ components/Author.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <slot name="photo"></slot>
+        <slot name="name"></slot>
+        <slot name="description"></slot>
+    </div>
+</template>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,6 +1,30 @@
 <template>
     <div>
-        impressum
- 
+        <div>
+            <h1>Welcome</h1>
+            <h2>This page was created by: </h2>
+        </div>
+        <Author>
+            <p slot="photo"> Photo </p>
+            <p slot="name"> Mariusz Trzeciakiewicz </p>
+            <p slot="description"> Computer science student at TUB </p>
+        </Author>
+
+        <Author>
+            <p slot="photo"> Photo </p>
+            <p slot="name"> Karol Rogoza </p>
+            <p slot="description"> Computer science student at TUB </p>
+        </Author>
     </div>
 </template>
+
+<script>
+//import Author from '@/components/Author.vue'
+
+export default {
+    components: {
+        
+
+    }
+}
+</script>
