05555618101c7c185cfea1180639d3b1ce4dc404
Marii19 <m.trzeciakiewicz14@gmail.com>
2020-05-25 13:14:36 +0200

    get Todos api

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,13 +5,9 @@
 
     <div class="sm:w-max-400px m-auto mb-24">
       <!--TODO component 3props, todo_name, date, procent -->
-      <Todo todo_name="Put together an epic puzzle." date="05.02.2020"  procent="22 %"/>
-      <Todo todo_name="Watch a rom-com marathon." date="06.12.2021"  procent="75 %"/>
-      <Todo todo_name="Sing along to some Disney songs." date="06.12.2021"  procent="23 %"/>
-      <Todo todo_name="Get a workout in." date="06.12.2021" procent="90 %"/>
-      <Todo todo_name="Write a letter." date="06.12.2021" procent="10 %"/>
-      <Todo todo_name="Call a friend." date="06.12.2021" procent="40 %"/>
-      <Todo todo_name="Have a dance party." date="06.12.2021" procent="66 %"/>
+      
+      <Todo v-for="(todo,idx) in todos" v-bind:key="idx" :todo_name="todo.title" :date="todo.deadline" :procent="todo.procent"/>
+
       
     </div>
   </div>
@@ -25,10 +21,29 @@ import Title from '@/components/Title'
 import Todo from '@/components/Todo'
 import Vue from 'vue'
 
+
+// import service to make api call
+import TodoService from '@/services/TodoService.js'
+
 export default Vue.extend({
+  data() {
+    return{
+      todos: []
+    }
+  },
   components: {
     Title,
     Todo,
+  },
+  created() {
+    
+    const response =  TodoService.getTodos()
+    .then(response => {
+      this.todos=response.data
+    })
+  }, 
+  methods: {
+    
   }
 })
 </script>
---
+++ frontend/services/TodoService.js
@@ -0,0 +1,17 @@
+//json-server --watch db.json
+import axios from 'axios'
+
+const apiClient = axios.create({
+   baseURL: 'http://localhost:3000',
+   withCredentials: false,
+   headers: {
+       Accept: 'application/json',
+       'Content-Type': 'application/json'
+   } 
+})
+
+export default {
+    getTodos() {
+        return apiClient.get('/todos',{withCredentials: true})
+    }
+}
